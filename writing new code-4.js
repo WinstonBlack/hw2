0628b6f48907748c260966c51cd713a850a24b40
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-01-28 21:20:12 -0800

    Update writing new code-4.js

--- writing new code-4.js
+++ writing new code-4.js
@@ -1,1 +1,16 @@
-
+function setup() { 
+  createCanvas(100, 100);
+strokeWeight(3);
+stroke(51);
+rect(20, 20, 60, 60);
+	strokeWeight(3);
+stroke(51);
+rect(1, 1, 20, 20);
+	strokeWeight(3);
+stroke(51);
+rect(1, 20, 20, 40);
+	strokeWeight(3);
+stroke(51);
+ fill(240, 100, 100);
+rect(20, 20, 20, 60);
+} 
