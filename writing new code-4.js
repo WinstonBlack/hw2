14df8509aec90de619d781b06a07713a1ca2aae7
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-01-28 21:26:37 -0800

    Update writing new code-4.js

--- writing new code-4.js
+++ writing new code-4.js
@@ -1,16 +1,22 @@
-function setup() { 
-  createCanvas(100, 100);
-strokeWeight(3);
-stroke(51);
-rect(20, 20, 60, 60);
+function setup() {
+	createCanvas(100, 100);
 	strokeWeight(3);
-stroke(51);
-rect(1, 1, 20, 20);
+	stroke(51);
+	rect(20, 20, 60, 60); // rect 1
 	strokeWeight(3);
-stroke(51);
-rect(1, 20, 20, 40);
+	stroke(51);
+	rect(1, 1, 20, 20); // rect 2
 	strokeWeight(3);
-stroke(51);
- fill(240, 100, 100);
-rect(20, 20, 20, 60);
-} 
+	stroke(51);
+	rect(1, 20, 20, 40); // rect 3
+	strokeWeight(3);
+	stroke(51);
+	fill(240, 100, 100);
+	rect(20, 20, 20, 60); // rect 4
+	stroke(51);
+	fill(50, 100, 100);
+	rect(20, 1, 60, 20); // rect 5
+	strokeWeight(3);
+	stroke(51);
+	rect(20, 1, 20, 20) //// rect 6
+}
