ba044d8fbae6ea75d7d3f51c610e1601a5fab14d
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-01-29 13:33:32 -0800

    Update writing new code-4.js

--- writing new code-4.js
+++ writing new code-4.js
@@ -1,22 +1,44 @@
 function setup() {
 	createCanvas(100, 100);
+	colorMode(HSB);
 	strokeWeight(3);
-	stroke(51);
+	stroke(31);
+	
 	rect(20, 20, 60, 60); // rect 1
 	strokeWeight(3);
-	stroke(51);
+	stroke(31);
+	
 	rect(1, 1, 20, 20); // rect 2
 	strokeWeight(3);
-	stroke(51);
+	stroke(31);
+	
 	rect(1, 20, 20, 40); // rect 3
 	strokeWeight(3);
-	stroke(51);
-	fill(240, 100, 100);
+	stroke(31);
+	fill(240, 100, 0);
+	
 	rect(20, 20, 20, 60); // rect 4
-	stroke(51);
-	fill(50, 100, 100);
+	stroke(31);
+	fill(360, 100, 100);
+	
 	rect(20, 1, 60, 20); // rect 5
 	strokeWeight(3);
-	stroke(51);
+	stroke(31);
+	
 	rect(20, 1, 20, 20) //// rect 6
+	strokeWeight(3);
+	
+	stroke(31);
+	fill(200, 100, 100);
+	rect(1, 50, 20, 30) //// rect 7
+	
+	stroke(31);
+	noFill();
+	rect(40, 50, 40, 30); // rect 8
+	
+	stroke(31);
+	fill(60, 100, 100);
+	rect(60, 50, 20, 30); // rect 9
+	
+	
 }
