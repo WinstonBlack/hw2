537e0381f956f9f4bdd5b9af94bea70ae1fc60ec
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-01-29 14:14:10 -0800

    Update lines-2.js

--- lines-2.js
+++ lines-2.js
@@ -21,12 +21,31 @@ function draw() {
 // (C)
 
 function setup() {
-  createCanvas(340, 340);
+	createCanvas(340, 340);
 }
 
 function draw() {
- line(40,120,80,60);
+	var x1 = random(width);
+	
+	var x2 = random(width);
+	var y2 = random(height);
+	var y1 = 2*(x1 - x2) + y2
+	line(x1, y1, x2, y2);
+
 }
 
 // (D)
-?
+function setup() {
+	createCanvas(340, 340);
+	colorMode(HSB);
+}
+
+function draw() {
+	var x1 = random(width);
+	stroke(x1,100, 100);
+	var x2 = random(width);
+	var y2 = random(height);
+	var y1 = 2 * (x1 - x2) + y2;
+	line(x1, y1, x2, y2);
+
+}
