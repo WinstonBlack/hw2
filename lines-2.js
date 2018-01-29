fd819082c6aa11efe848aea6c01cbcfe76e04d51
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-01-28 19:58:40 -0800

    Create lines-2.js

---
+++ lines-2.js
@@ -0,0 +1,30 @@
+// (A)
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+ line(170,170,random(width), random(height));
+}
+
+// (B)
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+ line(random(width), random(height),340,0);
+}
+
+// (C)
+
+function setup() {
+  createCanvas(340, 340);
+}
+
+function draw() {
+ line(40,120,80,60);
+}
+
