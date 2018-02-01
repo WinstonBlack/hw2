d789838fe3a66d705d53dd6fe417c0cbb0019656
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-02-01 12:16:46 -0800

    Update zelda.js

--- zelda.js
+++ zelda.js
@@ -4,13 +4,19 @@ function yourPatch(x, y) {
   noFill();
   stroke(238);
   rect(x, y, 300, 300);
-  
-triangle(x+150, y+100, x+100, y+200, x+200, y+200);
-triangle(x+150, y+200, x+125, y+150, x+175, y+150);// your code here!
+  colorMode(HSB);
+  background(0);
+
+  noStroke();
+  fill(45, 100, 100);  
+  triangle(x+150, y+100, x+100, y+200, x+200, y+200); // triangle 1
+
+  noStroke();
+  fill(45, 100, 0); 
+  triangle(x+150, y+200, x+125, y+150, x+175, y+150);// triangle 2
+ 
 }
 
 background(255);
 yourPatch(0, 0); // draw patch at upper-left
 
-background(255);
-yourPatch(width-301, height-301); // draw patch at lower-right
